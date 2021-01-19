39372fa5b74db80fc700522a3e9a6f27397b11b0
Song Eun Seok <eunsuk93@naver.com>
2021-01-19 18:59:51 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -269,6 +269,8 @@ def main():
         # ===Modify codes below=============
         # 위의 코드를 포함하여 자유로운 수정이 가능함
         user_input = input("Input guess number : ")
+        if user_input == '0':
+            break
         if not is_validated_number(user_input):
             print("Wrong Input, Input again")
             continue
